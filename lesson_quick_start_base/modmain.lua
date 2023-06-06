94bba55932dbc5a086fafe76ccaa0ebf7b0fd4b9
Dershine <69829662+Dershine@users.noreply.github.com>
2023-06-06 13:44:24 +0800

    final_lotus_umbrella

--- lesson_quick_start_base/modmain.lua
+++ lesson_quick_start_base/modmain.lua
@@ -2,6 +2,9 @@ PrefabFiles = {
     "lotus_umbrella",
 }
 
+env.RECIPETABS = GLOBAL.RECIPETABS
+env.TECH = GLOBAL.TECH
+
 local recipe_name = "lotus_umbrella"
 local ingredients = {Ingredient("cutgrass", 1), Ingredient("twigs", 1)}
 local tech = TECH.NONE --所需科技，必须使用常量表TECH的值
@@ -12,5 +15,5 @@ AddRecipe2(recipe_name, ingredients, tech, config)
 
 env.STRINGS = GLOBAL.STRINGS
 STRINGS.NAMES.LOTUS_UMBRELLA = "荷叶伞"
-STRINGS.CHARACTERS.GRNERIC.DESCRIBE.LOTUS_UMBRELLA = "这伞能挡雨吗"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.LOTUS_UMBRELLA = "这伞能挡雨吗"
 STRINGS.RECIPE_DESC.LOTUS_UMBRELLA = "荷叶做的雨伞" -- 制作栏描述
