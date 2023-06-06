acb343c96f0f31193ed2cfd6bec7c88df8ae0104
Dershine <69829662+Dershine@users.noreply.github.com>
2023-06-06 17:03:21 +0800

    1

---
+++ month_card_bag/modinfo.lua
@@ -0,0 +1,21 @@
+name = "month_card_bag"
+author = "dershines"
+description = [[
+月卡，每日获得一个奖励包，包含200合成玉和一个理智液
+]]
+version = "1.0.0"
+dst_compatible = true
+forge_compatible = false
+gorge_compatible = false
+dont_starve_compatible = false
+client_only_mod = false
+all_clients_require_mod = true
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+forumthread = ""
+api_version_dst = 10
+priority = 0
+mod_dependencies = {}
+server_filter_tags = {}
+configuration_options = {
+}
